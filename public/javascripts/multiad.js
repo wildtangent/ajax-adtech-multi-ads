75518f7500a81c9fb794172d4b833eb74be95f8a
Joe Connor <joe.connor@factorymedia.com>
2013-10-26 22:27:51 +0100

    add some comments

--- public/javascripts/multiad.js
+++ public/javascripts/multiad.js
@@ -1,11 +1,18 @@
 $(function(){
   
+  // Load ads with AJAX
   var loadAds = function(){
+    
+    // Set the adgroupid for some reason
     if (window.adgroupid == undefined) {
     	window.adgroupid = Math.round(Math.random() * 1000);
     }
+    
+    // Definw the ad server request
     var adRequestUrl = "http://adserver.adtech.de/multiad/3.0/25/0/0/-1/ADTECH;mode=json;cors=yes;plcids=2154536,2130496,2130497;loc=100;target=_blank;grp=" + window.adgroupid + ";cookie=info;misc=" + new Date().getTime();
     
+    
+    // Make the AJAX request
     $.ajax({
       url: adRequestUrl, 
       dataType: "json",
@@ -13,13 +20,20 @@ $(function(){
       cache: false,
       crossdomain: true,
       success:  function(data, status, xhr){
+        
+        // Loop through each ad unit
         $.each(data.ADTECH_MultiAd, function(i, item){
           var adSlot = "#ad_" + (i + 1);
+          
+          // Wrap the ad tag in a script tag so it gets processed correctly
           var adTag = "<script>" + item.Ad.AdCode + "</script>";
+          
+          // Process with postscribe
           postscribe(adSlot, adTag, {
             beforeWrite: function(str){
-             $(adSlot).html("");
-             return str;
+              // Clear the current ad
+              $(adSlot).html("");
+              return str;
             }
           });
         })
@@ -27,6 +41,7 @@ $(function(){
     });
   }
   
+  // Bind the reload button event
   var bindReload = function(){
     $("a#reload").on("click", function(e){
 
@@ -35,12 +50,9 @@ $(function(){
       loadAds();
     })
   }
-  
+  bindReload();
   
   // Just setting a delay so you can see the Async working
-  bindReload();
   setTimeout(loadAds, 1000);
-  
-  
-  
+    
 })
